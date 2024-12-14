b2e053041d56eef76f6a4951a43411ba1fca3646
tranzbook <tranzbook1@gmail.com>
2024-12-14 09:11:03 +0000

    eslint

--- lib/db.ts
+++ lib/db.ts
@@ -1,19 +1,17 @@
+/* eslint-disable no-var, @typescript-eslint/no-explicit-any */
+
 import { PrismaClient } from '@prisma/client';
 
-// Singleton function to initialize the Prisma client
 const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
-// Extend the TypeScript globalThis type to include prismaGlobal
 declare global {
-  var prismaGlobal: PrismaClient | undefined;
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV !== 'production') {
-  globalThis.prismaGlobal = prisma;
-}
-
 export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
