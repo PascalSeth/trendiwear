22cde167643c87a5e73ab858876113b4b0b182c4
tranzbook <tranzbook1@gmail.com>
2024-12-26 07:53:20 +0000

    sessioj

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -2,26 +2,34 @@ import prisma from "@/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function GET(){
-const{getUser}=getKindeServerSession()
-const user = await getUser()
+export async function GET() {
+    if (!process.env.NEXT_RUNTIME) {
+        return new Response("This API route is not accessible during static generation.", {
+            status: 403,
+        });
+    }
 
-if(!user || user === null|| !user.id){
-throw new Error('User not found')
-}
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
 
-let dbUser= await prisma.user.findUnique({
-where:{id:user.id}
-})
-if (!dbUser){
-    dbUser = await prisma.user.create({
-        data:{
-            id:user.id,
-            email:user.email??'',
-            name:user.given_name??'',
-            profileImage:user?.picture??`https://avatar.vercel.sh/${user.given_name}`}
-    })
-}
-return NextResponse.redirect('https://trendiwear.netlify.app/')
+    if (!user || !user.id) {
+        throw new Error("User not found");
+    }
 
+    let dbUser = await prisma.user.findUnique({
+        where: { id: user.id },
+    });
+
+    if (!dbUser) {
+        dbUser = await prisma.user.create({
+            data: {
+                id: user.id,
+                email: user.email ?? "",
+                name: user.given_name ?? "",
+                profileImage: user?.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
+            },
+        });
+    }
+
+    return NextResponse.redirect("https://trendiwear.netlify.app/");
 }
